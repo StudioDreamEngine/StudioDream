dae5cc1ac8befc755d0dcb86a17b381959b8ba58
Mikl <91346810+Milk2010@users.noreply.github.com>
2026-03-30 19:53:31 -0300

    Feature: Added inicial globals init

---
+++ src/Runtime/Globals/init.lua
@@ -0,0 +1,64 @@
+local Main = {}
+local ChangedSignals = {}
+
+Globals = {}
+
+CurrentGlobals = {
+    "Envorioment",
+    "UI",
+    "Scripts",
+    "Assets"
+}
+
+local function FireChanged(WhatChanged,WhereChanged)
+    for i,v in ChangedSignals do
+        v(WhatChanged,WhereChanged)
+    end
+end
+
+function Main.AddGlobal(Name)
+    Globals[i] = setmetatable({},{
+        __index = function(WhatChanged)
+            local Globs = rawget(Globals)
+
+            FireChanged(WhatChanged,"Global") -- change this later to whatever the parent of the table is
+
+            return Globs
+        end
+    })
+end
+
+function Main.AddThingToGlobal(Where,What) -- nake this suport trails (aka Example.Example.Example) and aways add as a child
+    if Globals[Where] then
+        return Globals[Where][What] = {}
+    else
+        error(`Coulnt find `..Where..` Global.`)
+    end
+end
+
+function Main.NotifyGlobalChange()
+    local self = {}
+    local ChangeSignal = Signal:New("GlobalChanged")
+    local WhatChanged
+    local WhereChanged
+
+    function self:Connect(func)
+        table.insert(ChangedSignals,ChangeSignal)
+    end
+    
+    return self
+end
+
+function Main.GlobalUpdate()
+    for i,v in CurrentGlobals do
+        Main.AddGlobal(i)
+    end
+end
+
+function Main.Init()
+    Main.GlobalUpdate()
+
+
+end
+
+return Main
