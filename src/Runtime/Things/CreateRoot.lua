d2b3412c05f7f538b9c8f2a9783b8d90727a6284
Bloctans <93960460+Bloctans@users.noreply.github.com>
2026-04-05 18:40:04 -0500

    fix: viewport size

--- src/Runtime/Things/CreateRoot.lua
+++ src/Runtime/Things/CreateRoot.lua
@@ -8,14 +8,10 @@ return function()
     Viewport.ExplorerVisible = false
     Viewport.Size = Pivot2D.FromOffset(Vector2.new(640, 480))
 
-    local SquareTest = Things.New("SquarePrimative")
-    SquareTest.Size = Pivot2D.FromOffset(Vector2.new(200,200))
-    SquareTest:SetParent(Viewport)
-
     local ImageTest = Things.New("Image2D")
     ImageTest.ImageFile = "Editor/Assets/Icons/16/application_edit.png"
     ImageTest.Size = Pivot2D.FromOffset(Vector2.new(100,100))
-    ImageTest.Position = Pivot2D.FromOffset(Vector2.new(100,100))
+    ImageTest.Position = Pivot2D.FromOffset(Vector2.new(40,40))
     ImageTest:SetParent(Viewport)
 
     return { Viewport = Viewport }
