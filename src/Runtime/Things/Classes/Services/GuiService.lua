d48aecab8334eb7d988a7fe79afab14560598512
Bloctans <93960460+Bloctans@users.noreply.github.com>
2026-04-06 20:25:49 -0500

    move backend

---
+++ src/Runtime/Things/Classes/Services/GuiService.lua
@@ -0,0 +1,6 @@
+local Things = Runtime.Things
+local GuiService = Things.Extend("Thing")
+
+function GuiService:Update(dt) end
+
+return GuiService
--- src/Runtime/init.lua
+++ src/Runtime/init.lua
@@ -2,9 +2,12 @@ local Runtime = {}
 
 Runtime.Things = require("Runtime.Things")
 Runtime.Renderer = require("Runtime.Renderer")
-Runtime.Backend2D = require("Runtime.LoveBackend")
+Runtime.Backend = require("Runtime.Backend")
 
 function Runtime.Init()
+    Runtime.Backend.Init()
+    Runtime.Backend2D = Runtime.Backend.Get2DBackend()
+
     Runtime.Renderer.Init()
     Runtime.Things.Init()
 end
