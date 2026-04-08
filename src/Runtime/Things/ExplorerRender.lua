03d04859b1fe3ecb07d299c3c4373ac3b96ad4ea
Mikl <91346810+Milk2010@users.noreply.github.com>
2026-04-08 16:26:40 -0300

    feat: Updated Explorer containers

--- src/Runtime/Things/ExplorerRender.lua
+++ src/Runtime/Things/ExplorerRender.lua
@@ -8,6 +8,10 @@ local RowHeight = 24
 local IndentSize = 16
 local IconsSize = 16 -- i will change this when we actually start making icons frfr
 
+local NodeColor = Color.new(0.314, 0.294, 0.502)
+local WindowColor = Color.new(0.106, 0.09, 0.188)
+local BackWindowColor = Color.new(0.149, 0.129, 0.333)
+
 local function RenderIcon(IconName, VectorPos, Container)
     local ImageThing = Things.New("Image2D")
     ImageThing.Size = Pivot2D.FromOffset(Vector2.one * IconsSize)
@@ -39,12 +43,16 @@ local function RenderNode(Thing, currentY, depth ,XPos)
     local iconPos  = Vector2.new(xOffset, currentY)
     local labelPos = Vector2.new(xOffset + IconsSize + 4, currentY)
 
-    local NodeContainer = Things.New("SquarePrimative")
-    NodeContainer.Size = Pivot2D.FromOffset(Vector2.new(200, 20))
-    NodeContainer.Position = Pivot2D.FromOffset(xOffset,currentY)
-    NodeContainer.Explorer.Visible = false
-    NodeContainer.BackgroundColor = Color.new(0.137, 0.129, 0.2)
-    NodeContainer.Layer = 1
+    local NodeContainer = Things.Create "SquarePrimative" {
+       Size = Pivot2D.FromOffset(Vector2.new(200, 20)),
+       Position = Pivot2D.FromOffset(xOffset,currentY),
+       Explorer = {
+        Visible = false,
+       },
+       BackgroundColor = NodeColor,
+       Layer = 1,
+       BackgroundTransparency = 0.9
+    }
     NodeContainer:SetParent(ExplorerContainer)
 
     local icon = Thing.Explorer.Icon or "cancel"
@@ -62,25 +70,31 @@ end
 return function()
     local TreeStarter = Things.Root.Viewport
 
-     ---@module 'SquarePrimative'
-    ExplorerContainer1 = Things.New("SquarePrimative")
-    ExplorerContainer1.BackgroundColor = Color.new(0.376, 0.365, 0.443)
-    ExplorerContainer1.Size = Pivot2D.FromOffset(Vector2.new(200,500))
-    ExplorerContainer1.Explorer.Visible = false
-    ExplorerContainer1.Name = "Explorer"
-    ExplorerContainer1.Position = Pivot2D.FromOffset(Vector2.new(570,50))
+    ExplorerContainer1 = Things.Create "SquarePrimative" {
+       Size = Pivot2D.FromOffset(Vector2.new(200,500)),
+       Position = Pivot2D.FromOffset(Vector2.new(570,50)),
+       Explorer = {
+        Visible = false,
+       },
+       BackgroundColor = WindowColor,
+       Name = "Explorer",
+       Layer = 1,
+    }
     ExplorerContainer1:SetParent(TreeStarter)
 
     RenderTextLabel("~-Explorer-~", Vector2.new(0,0), ExplorerContainer1,Pivot2D.FromScale(Vector2.new(5,5)),12)
 
-    ---@module 'SquarePrimative'
-    ExplorerContainer = Things.New("SquarePrimative")
-    ExplorerContainer.BackgroundColor = Color.new(0.196, 0.184, 0.29)
-    ExplorerContainer.Size = Pivot2D.FromOffset(Vector2.new(190,250))
-    ExplorerContainer.Explorer.Visible = false
-    ExplorerContainer.Name = "Explorer"
-    ExplorerContainer.Layer = 2
-    ExplorerContainer.Position = Pivot2D.FromOffset(Vector2.new(5,20))
+    ExplorerContainer = Things.Create "SquarePrimative" {
+       Size = Pivot2D.FromOffset(Vector2.new(190,250)),
+       Position = Pivot2D.FromScale(0.5,0.3),
+       Pivot = Vector2.new(0.5,0.5),
+       Explorer = {
+        Visible = false,
+       },
+       BackgroundColor = BackWindowColor,
+       Name = "BackWindow",
+       Layer = 2,
+    }
     ExplorerContainer:SetParent(ExplorerContainer1)
 
     RenderNode(TreeStarter, 0, 0, 0)
