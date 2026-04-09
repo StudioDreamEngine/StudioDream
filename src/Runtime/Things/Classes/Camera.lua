c9e80d93ff700ef07cd40bc118f580707008d263
Mikl <91346810+Milk2010@users.noreply.github.com>
2026-04-09 16:37:10 -0300

    feat: Some adjustments to the UI and Camera Thing

---
+++ src/Runtime/Things/Classes/Camera.lua
@@ -0,0 +1,21 @@
+local Things = Runtime.Things
+
+-- using @module here gives the lua language server a base type to use!
+---@module 'BaseGui'
+
+local Camera = Things.Extend("Thing")
+
+function Camera:new()
+    Camera.super.new(self)
+
+    self.Explorer = {
+        Visible = true,
+        Icon = "camera"
+    }
+end
+
+function Camera:Update(dt)
+
+end
+
+return Camera
