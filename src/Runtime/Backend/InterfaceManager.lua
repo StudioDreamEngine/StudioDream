453ee27d404eb15b7a7797d077cc480755062736
Mikl <91346810+Milk2010@users.noreply.github.com>
2026-04-11 22:19:09 -0300

    nothin

--- src/Runtime/Backend/InterfaceManager.lua
+++ src/Runtime/Backend/InterfaceManager.lua
@@ -30,4 +30,35 @@ function InterfaceManager.Update(dt)
     InterfaceManager.UpdateInput()
 end
 
+function InterfaceManager.CreateWindow(Size)
+    local Windows = {}
+    Windows.Container = Things.Create "SquarePrimative" { 
+      Size = Size,
+       Explorer = {
+        Visible = false,
+       },
+       Position = Pivot2D.FromScale(0,0.78),
+       Pivot = Vector2.new(0,0.5),
+       BackgroundColor = WindowColor,
+       Name = "Properties",
+       Layer = 1,
+       Parent = View
+    }
+    
+    Windows.BackWindow = Things.Create "SquarePrimative" {
+       Size = Pivot2D.FromScale(0.9,0.9),
+       Position = Pivot2D.FromScale(0.5,0.5),
+       Pivot = Vector2.new(0.5,0.5),
+       Explorer = {
+        Visible = false,
+       },
+       BackgroundColor = BackWindowColor,
+       Name = "BackWindow",
+       Layer = 2,
+       Parent = PropertiesContainer
+    }
+
+    return Windows
+end
+
 return InterfaceManager
