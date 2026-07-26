e095e1c395ee39c74fec1f29913ce757b0e8ebc5
Bloctans <bloctans2@protonmail.com>
2026-07-26 11:07:47 -0500

    fix: fallback script on compilation failure

--- src/Runtime/Resources/Loaders/Script.lua
+++ src/Runtime/Resources/Loaders/Script.lua
@@ -1,5 +1,19 @@
 return function(ScriptBytes, Identifier)
     local Contents = "return function()\n"..ScriptBytes.."\nend"
+    
+    local Function, Error = load(Contents, Identifier.ID, "t", {})
 
-    return load(Contents, Identifier.ID, "t", {})
+    if (not Function) then
+        return load([[
+            return function()
+                print("Failed to compile script (]]..Identifier.ID..[[): "]]..Error..[[)
+
+                return "Compilation failure"
+            end
+        ]], Identifier.ID, "t", {
+            print = print
+        })
+    end
+
+    return Function
 end
