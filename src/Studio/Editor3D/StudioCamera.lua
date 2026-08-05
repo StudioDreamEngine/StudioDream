ca6126efc6f310d8e7696bfbce7f9b272af513b5
sonickirb <sonicyan11@gmail.com>
2026-08-05 17:22:15 -0500

    fix fuckass broken camera :3

--- src/Studio/Editor3D/StudioCamera.lua
+++ src/Studio/Editor3D/StudioCamera.lua
@@ -3,6 +3,7 @@ local StudioCamera = {}
 local HoldingCamera = false
 local CameraRotation = Vector2.zero
 local CameraPosition = Vector3.zero
+local MouseDelta = Vector2.zero
 
 local MouseService = Runtime.Services.Service("MouseService") ---@class MouseService
 local InputService = Runtime.Services.Service("InputService") ---@class InputService
@@ -15,6 +16,7 @@ function StudioCamera.Init()
             MouseService.SetMouseMode(Enum.MouseMode.Locked)
         else
             MouseService.SetMouseMode(Enum.MouseMode.Free)
+            MouseDelta = Vector2.zero
         end
 
         HoldingCamera = IsDown
@@ -24,8 +26,7 @@ function StudioCamera.Init()
         if (not HoldingCamera) then return end
 
         local Delta = MouseObject.Delta
-        CameraRotation.X = CameraRotation.X + Delta.X/300 
-        CameraRotation.Y = CameraRotation.Y - Delta.Y/300
+        MouseDelta = Delta
     end)
 end
 
@@ -43,6 +44,10 @@ function StudioCamera.Update(dt)
         local Forward = Camera.Transform.Forward * (KeyDownNum(Enum.InputCode.S) - KeyDownNum(Enum.InputCode.W))
         local Side = Camera.Transform.Side * (KeyDownNum(Enum.InputCode.D) - KeyDownNum(Enum.InputCode.A))
         local Direction = (Forward + Side).Unit()
+
+        -- TODO: fix this fuckass random 200 number value >:3
+        CameraRotation.X = CameraRotation.X + MouseDelta.X/200
+        CameraRotation.Y = CameraRotation.Y - MouseDelta.Y/200
 
         CameraPosition = CameraPosition + Direction*dt*3
 
