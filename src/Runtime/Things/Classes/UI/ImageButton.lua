184484eb75689526b99ea2e6f5c4a6adc6a2ad84
sonickirb <sonicyan11@gmail.com>
2026-08-06 16:42:50 -0500

    add hover cursors to ImageButton

--- src/Runtime/Things/Classes/UI/ImageButton.lua
+++ src/Runtime/Things/Classes/UI/ImageButton.lua
@@ -17,7 +17,10 @@ function ImageButton:new()
     self.ClickingColorMultiplier = 0.5
 
     self.Clicked = Signal:New("ButtonClicked")
-    self.RightClicked = Signal:New("ButtonRightClicked")
+    self.RightClicked = Signal:New("ButtonRightClicked") -- why isnt this in TextButton too???? x3
+    self.HoverEnter = Signal:New("ButtonHoverEnter")
+    self.HoverExit  = Signal:New("ButtonHoverExit")
+    self._WasHovering = false
 
     Runtime.InterfaceManager.OnClick:Connect(function()
         if not self.Hovering or not self:IsVisible() then return end
@@ -28,6 +31,16 @@ function ImageButton:new()
         if not self.Hovering or not self:IsVisible() then return end
         
         self.RightClicked.Invoke()
+    end)
+
+    -- "quote comment from TextButton here" :3
+    self.HoverEnter:Connect(function()
+        if Runtime.Cursor.CurrentCursor == 'HoldingObj' then return end
+        Runtime.Cursor.ChangeCursor("Hovering")
+    end)
+    self.HoverExit:Connect(function()
+        if Runtime.Cursor.CurrentCursor == 'HoldingObj' then return end
+        Runtime.Cursor.ChangeCursor("Main")
     end)
 end
 
@@ -55,11 +68,17 @@ end
 function ImageButton:Update(dt)
     ImageButton.super.Update(self)
 
+    -- why is there TWO of them??? :!#$0o3$?AS"?#$%P!@)#(@)#$%(O :3
     local Clicking = self.Hovering and Runtime.InterfaceManager.Clicking
 
     local Clicking = self.Hovering and Runtime.InterfaceManager.Clicking
     local Multiplier = (Clicking and self.ClickingColorMultiplier) or (self.Hovering and self.HoverColorMultiplier) or 1
     self.ColorMultiplier = Multiplier
+
+    if (not self._WasHovering and self.Hovering) then self.HoverEnter.Invoke() end
+    if (self._WasHovering and not self.Hovering) then self.HoverExit.Invoke() end
+
+    self._WasHovering = self.Hovering
 end
 
 return ImageButton
