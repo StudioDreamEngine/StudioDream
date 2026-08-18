9a69779e456ea54e20c1ef41160dd5b2b9f71fbd
Mikl <91346810+Milk2010@users.noreply.github.com>
2026-08-18 19:31:34 -0300

    feat: Verbose enabled now

--- src/flags.lua
+++ src/flags.lua
@@ -1,6 +1,6 @@
 return {
     Target = "Studio", -- What this build's functionality should be, disables studio component if "ClientRuntime", enables studio if "Editor"
-    Verbose = false,
+    Verbose = true,
     DebugDraw = false,
     ExternalOutput = true,
     Independent = false,
