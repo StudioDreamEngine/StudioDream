8af64a5050a9d553e82b9d4fff090ce03bcac083
Mikl <91346810+Milk2010@users.noreply.github.com>
2026-08-18 19:32:40 -0300

    feat: nvm ig

--- src/flags.lua
+++ src/flags.lua
@@ -1,6 +1,6 @@
 return {
     Target = "Studio", -- What this build's functionality should be, disables studio component if "ClientRuntime", enables studio if "Editor"
-    Verbose = true,
+    Verbose = false,
     DebugDraw = false,
     ExternalOutput = true,
     Independent = false,
